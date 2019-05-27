bd18d20c601aef3c5688a738553713cc4491b9e9
Jonathan Higgins <jonathan.higgins1@MARIST.EDU>
2019-05-27 19:28:28 -0400

    HelloWorld
    Java HelloWorld

---
+++ Java/SetupTest/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//This is a test to ensure that I installed Eclipse Photon Properly.
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print 'Hello' on a new line.
+		System.out.println("Hello");
+
+	}
+
+}
+
