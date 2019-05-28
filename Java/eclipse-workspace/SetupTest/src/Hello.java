7cec5fe2ddd78459cfa093fba3846f289d1bdb34
Jonathan Higgins <jonathan.higgins1@MARIST.EDU>
2019-05-27 21:20:07 -0400

    Java Hello World
    Hello World for Photon set-up

---
+++ Java/eclipse-workspace/SetupTest/src/Hello.java
@@ -0,0 +1,11 @@
+//This is a test to ensure that I installed Eclipse Photon Properly.
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print 'Hello' on a new line.
+		System.out.println("Hello");
+
+	}
+
+}
