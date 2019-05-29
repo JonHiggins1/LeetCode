5f8b79067ff906a077d06ed87b00dd817b2f506e
Jonathan Higgins <jonathan.higgins1@MARIST.EDU>
2019-05-28 21:27:17 -0400

    5/28
    testing some more java

---
+++ Java/SetupTest/src/Hello2.java
@@ -0,0 +1,24 @@
+/**
+ * Training
+ * @author JH
+ * JavaDoc comment about the class.
+ *
+ */
+public class Hello2 {
+	
+	/**
+	* Document comment for the main method.
+	**/
+
+	public static void main(String[] args)  //method header
+	{
+		System.out.print ("Hello Java, ");
+		System.out.println ("version 2");
+		System.out.println ("Seperate print line");
+		/*
+		 Multi-line comments CANNOT NEST
+		 */
+
+	}
+
+}
